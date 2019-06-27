45731edc51b2b60ea7a3d110c10a52a517e809d1
Álvaro <alvaritovg93@hotmail.com>
2019-06-27 20:06:04 +0200

    Chat finalizado v1

--- public/js/socket-chat.js
+++ public/js/socket-chat.js
@@ -1,22 +1,26 @@
 var socket = io();
 
 var params = new URLSearchParams(window.location.search);
+
 if (!params.has('nombre') || !params.has('sala')) {
     window.location = 'index.html';
     throw new Error('El nombre y sala son necesarios');
 }
+
 var usuario = {
     nombre: params.get('nombre'),
     sala: params.get('sala')
 };
 
 
+
 socket.on('connect', function() {
     console.log('Conectado al servidor');
 
     socket.emit('entrarChat', usuario, function(resp) {
-        console.log('Usuarios conectados', resp);
+        renderizarUsuarios(resp);
     });
+
 });
 
 // escuchar
@@ -37,15 +41,19 @@ socket.on('disconnect', function() {
 
 // Escuchar información
 socket.on('crearMensaje', function(mensaje) {
-    console.log('Servidor:', mensaje);
+    renderizarMensajes(mensaje, false);
+    scrollBottom();
 });
 
-// Escuchar cuando un usuario entra o sale del chat
+// Escuchar cambios de usuarios
+// cuando un usuario entra o sale del chat
 socket.on('listaPersona', function(personas) {
-    console.log(personas);
+    renderizarUsuarios(personas);
 });
 
 // Mensajes privados
 socket.on('mensajePrivado', function(mensaje) {
-    console.log('Mensaje privado: ', mensaje);
+
+    console.log('Mensaje Privado:', mensaje);
+
 });
